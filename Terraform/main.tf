780161d8a6fef2dd2a3c45a98783395b0e563146
willamys <willamysx7@gmail.com>
2025-03-17 19:16:57 -0300

    Criando Terraform

---
+++ Terraform/main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  project = "meu-projeto-id"   # Substitua pelo seu ID do projeto GCP
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "minha_vm" {
+  name         = "minha-vm"
+  machine_type = "e2-micro"  # Tipo de máquina (econômico)
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"  # Imagem do SO
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      # Permite IP público
+    }
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    echo "Instalando Apache..."
+    sudo apt update -y
+    sudo apt install -y apache2
+    sudo systemctl start apache2
+    echo "Servidor Apache está rodando!" > /var/www/html/index.html
+  EOT
+}
